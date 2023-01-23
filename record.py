bbbd7f84a1691480a7dde8f4dc77d349e6475ae1
Raul Peinado <raulpeinado10@gmail.com>
2023-01-23 11:52:29 +0100

    Update record.py

--- record.py
+++ record.py
@@ -1,29 +1,17 @@
 import cv2
 import time
 import os
-
-#Iniciamos la camara
-cap= cv2.VideoCapture(0) 
-
-#Inicializamos los valores de la imagen
-cap.set(3,1280)
+    
+cap= cv2.VideoCapture(0)                                                        #Iniciamos la camara
+cap.set(3,1280)                                                                 #Inicializamos los valores de la imagen
 cap.set(4,720)
 width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-#Inicializamos el contador
-horas = time.ctime(time.time()).replace(' ','_').replace(':','-')
-
-#Inicializamos variables auxiliares
-cont_elimina = 0
+horas = time.ctime(time.time()).replace(' ','_').replace(':','-')               #Inicializamos el contador
+cont_elimina = 0                                                                #Inicializamos variables auxiliares
 cont_video = 0 
-
-#Seleccionamos el codec
-fourcc1 = cv2.VideoWriter_fourcc(*'mp4v') # fourcc2 =  cv2.VideoWriter_fourcc(*'DIVX')
-
-#Definimos el path donde se guardarán los videos
-path = './videos/'
-
+fourcc1 = cv2.VideoWriter_fourcc(*'mp4v')                                       #Seleccionamos el codec ### fourcc2 =  cv2.VideoWriter_fourcc(*'DIVX')
+path = './videos/'                                                              #Definimos el path donde se guardarán los videos
 while True:
     name = path+'video'+str(cont_video)+'-'+horas+'.mp4'
     writer= cv2.VideoWriter(name,fourcc1, 60, (width,height))
@@ -37,16 +25,13 @@ while True:
         if cont == int(time.ctime(time.time()).split(' ')[3].split(':')[0]): 
             horas = time.ctime(time.time()).replace(' ','_').replace(':','-') 
             break
-
-    if len(os.listdir(path))>23: #definimos el numero total de videos que se van a guardar
-        for p in os.listdir(path):
+    
+    if len(os.listdir(path))>23:                                                #Definimos el numero total de videos que se van a guardar (24)
+        for p in os.listdir(path):                                              #Eliminamos el vídeo más antiguo
             if 'video'+str(cont_elimina)+'-' in p:
                 os.remove(path+p)
+                break
         cont_video = cont_elimina
-        cont_elimina +=1
-        if cont_elimina == 24:
-            cont_elimina = 0
-    else: #guardamos los primeros videos
-        cont_video +=1
-        if cont_video ==24:
-            cont_video = 0
+        cont_elimina = 0 if cont_elimina==24 else cont_elimina+1
+    else:                                                                       #Almacenamos los primeros 24 vídeos
+        cont_video = 0 if cont_video == 24 else cont_video+1
